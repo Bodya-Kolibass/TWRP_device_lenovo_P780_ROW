58f5797897ae27da8292f673242b76be90796c58
Bodya-Kolibass <Mr.Kolibass@gmail.com>
2017-12-22 23:57:14 +0300

    Fix error7

--- patches/apply-patch.sh
+++ patches/apply-patch.sh
@@ -2,6 +2,7 @@
 
 cd bootable/recovery-twrp
 git apply -v ../../device/lenovo/P780_ROW/patches/0001-Fix-revert-color.patch
+git apply -v ../../device/lenovo/P780_ROW/patches/0002-WIPE-Don-t-leave-system-mounted-after-wipe.patch
 cd ../..
 
 echo Patch applied successfully
