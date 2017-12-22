d1e916f228991294d8a07aab6a405cf45a2083a4
Bodya-Kolibass <Mr.Kolibass@gmail.com>
2017-12-23 00:04:04 +0300

    Some GUI changes & custom settings by default

--- patches/apply-patch.sh
+++ patches/apply-patch.sh
@@ -3,6 +3,8 @@
 cd bootable/recovery-twrp
 git apply -v ../../device/lenovo/P780_ROW/patches/0001-Fix-revert-color.patch
 git apply -v ../../device/lenovo/P780_ROW/patches/0002-WIPE-Don-t-leave-system-mounted-after-wipe.patch
+git apply -v ../../device/lenovo/P780_ROW/patches/0003-Some-GUI-changes.patch
+git apply -v ../../device/lenovo/P780_ROW/patches/0004-Custom-settings-by-default.patch
 cd ../..
 
 echo Patch applied successfully
