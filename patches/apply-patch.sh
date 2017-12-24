0af679adf01a63c48c526c7324df2fa30a9606d4
Bodya-Kolibass <Mr.Kolibass@gmail.com>
2017-12-24 20:52:53 +0300

    Changes some Russian translation string

--- patches/apply-patch.sh
+++ patches/apply-patch.sh
@@ -6,6 +6,7 @@ git apply -v ../../device/lenovo/P780_ROW/patches/0002-WIPE-Don-t-leave-system-m
 git apply -v ../../device/lenovo/P780_ROW/patches/0003-Some-GUI-changes.patch
 git apply -v ../../device/lenovo/P780_ROW/patches/0004-Custom-settings-by-default.patch
 git apply -v ../../device/lenovo/P780_ROW/patches/0005-Fix-mass_storage-mode.patch
+git apply -v ../../device/lenovo/P780_ROW/patches/0006-Changes-some-Russian-translation-string.patch
 cd ../..
 
 echo Patch applied successfully
