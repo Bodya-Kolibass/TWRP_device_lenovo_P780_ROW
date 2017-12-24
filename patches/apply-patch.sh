674e6054edba10cdf58f4334ead1272624776fcc
Bodya-Kolibass <Mr.Kolibass@gmail.com>
2017-12-24 20:23:05 +0300

    Fix mass_storage mode

--- patches/apply-patch.sh
+++ patches/apply-patch.sh
@@ -5,6 +5,7 @@ git apply -v ../../device/lenovo/P780_ROW/patches/0001-Fix-revert-color.patch
 git apply -v ../../device/lenovo/P780_ROW/patches/0002-WIPE-Don-t-leave-system-mounted-after-wipe.patch
 git apply -v ../../device/lenovo/P780_ROW/patches/0003-Some-GUI-changes.patch
 git apply -v ../../device/lenovo/P780_ROW/patches/0004-Custom-settings-by-default.patch
+git apply -v ../../device/lenovo/P780_ROW/patches/0005-Fix-mass_storage-mode.patch
 cd ../..
 
 echo Patch applied successfully
